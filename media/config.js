c3fba940bbbf58de675532153d8b650a303fe208
DybyTech <dybytech00@gmail.com>
2024-12-09 22:50:44 -0500

    dyby

---
+++ media/config.js
@@ -0,0 +1,57 @@
+/*
+
+# Base By 𝕶𝖎𝖓𝖌 𝕾𝖆𝖒
+# Owner ? : 𝕶𝖎𝖓𝖌 𝕾𝖆𝖒
+!- do not delete this credit
+decode by tech god
+*/
+
+global.prefa = ['','!','.',',','🐤','🗿']
+global.owner = ['50934960331']
+global.ownMain = '50934960331'
+global.NamaOwner = '⏤͟͟͞͞𝄞∘̥⃟𝐄𝐆 𝐃𝐘𝐁𝐘 𝐓𝐄𝐂𝐇 🕸️' //
+global.sessionName = 'session'
+global.connect = true // 
+global.namabot = 'ᴍᴇɢᴀʟᴏᴅᴏɴ ᴍᴅ ʙᴜɢ ʙᴏᴛ' //
+global.author = '⏤͟͟͞͞𝄞∘̥⃟𝐄𝐆 𝐃𝐘𝐁𝐘 𝐓𝐄𝐂𝐇 🕸️' //
+global.packname = 'ᴍᴇɢᴀʟᴏᴅᴏɴ ᴍᴅ ʙᴜɢ ʙᴏᴛ' //
+global.url1 = 'https://whatsapp.com/channel/0029VaiOvsV1iUxQRXWnv40R' //
+global.url2 = 'https://whatsapp.com/channel/0029VaiOvsV1iUxQRXWnv40R' //
+global.linkgc = 'https://whatsapp.com/channel/0029VaiOvsV1iUxQRXWnv40R'
+global.delayjpm = 3500
+//Panel
+global.domain = 'https://guru.sellerpanell.me' // your domian
+global.apikey = 'ptla_aRq7aFBbQowvPwLChvjNLX5uE0rYZ6dUdGbFIhqUwUg' // 
+global.capikey = 'ptlc_mI4q2CFLyxYSG9lqtpWVJSkqtxbQvZyheemGqW56VUH' // 
+global.eggsnya = '15' // 
+global.location = '1' // 
+
+global.mess = { // 
+ingroup: 'This feature can only be used in groups.',
+admin: 'This feature is specifically for group admins.',
+notadmin: "The bot must be an admin first",
+owner: 'You are not owner.',
+premium: 'You are not a premium user.',
+seller: 'This feature can only be used by resellers and owners.',
+usingsetpp: `Setpp can only be used by the owner, do you think Im stupid?`,
+wait: '*Waiting for processing*',
+success: 'Success sent by ᴍᴇɢᴀʟᴏᴅᴏɴ ᴍᴅ ʙᴜɢ ʙᴏᴛ',
+bugrespon: `Processs.....`
+}
+
+global.nick = { // Custom Sesuka Lu
+aaa: "‏‎M҉A҉G҉A҉L҉O҉D҉O҉N҉ ҉B҉U҉G҉ ҉B҉O҉T҉  ",
+bbb: "⃰C҉ R҉ A҉ S҉ H҉ ̺",
+ccc: "M҉ A҉ S҉ T҉ E҉ R҉  M҉ I҉ N҉ D҉ 🔥 �? ‌‌‌‌‌‌‌‌‌‌�?",
+ddd: "K҉ I҉ N҉ G M҉A҉G҉A҉L҉O҉D҉O҉N҉ ҉B҉U҉G҉ ҉B҉O҉T҉    🌎",
+eee: "A҉ L҉ L҉  H҉ A҉ I҉ M҉A҉G҉A҉L҉O҉D҉O҉N҉ ҉B҉U҉G҉ ҉B҉O҉T҉ L҉ ҉ ҉ 🔥🔥"
+}
+
+global.autOwn = 'req(62-8S57547ms11).287p'
+let file = require.resolve(__filename)
+require('fs').watchFile(file, () => {
+	require('fs').unwatchFile(file)
+	console.log('\x1b[0;32m'+__filename+' \x1b[1;32mupdated!\x1b[0m')
+	delete require.cache[file]
+	require(file)
+})
